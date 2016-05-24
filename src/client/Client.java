94b43c152ec6a49d5a65bd34a098f1c6fb473bcd
Tomasz Bartnik <tomasz.bartnik93@gmail.com>
2016-05-24 08:30:08 +0200

    menu improved

--- src/client/Client.java
+++ src/client/Client.java
@@ -5,46 +5,41 @@ import client.EnterData;
 public class Client {
 	private static EnterData enter;
 	
-	static final String MENU = "\n\nM E N U\n" 
+	static final String MENU = "M E N U\n" 
 			+ "1 - wyswietl liste plikow\n"
 			+ "2 - dodaj plik\n"
 			+ "3 - usun plik\n"
 			+ "4 - pobierz plik\n"
-			+ "0 - zakoñcz program";
+			+ "0 - zakoñcz program\n";
 	
 	public static void main(String[] args) {
-
+		//new Client();
 		// Proszê wybraæ sposób komunikacji z u¿ytkownikiem
 		// "CON" - komunikacja z u¿ytkownikiem prowadzona w oknie konsoli
 		enter = new EnterData("CON");
 		// enter = new EnterData("GUI");
-		
-		System.out.println(MENU);
-		int size = enter.enterInt("Podaj swój wybór:");
-
-		//Client client = new Client();
-
+		int choice;
 		while (true) {
-			// kasowanie okna konsoli
-			// System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-
-			switch (enter.enterInt(MENU + "\n==>> ")) {
+			System.out.println(MENU);
+			choice = enter.enterInt("Podaj swój wybór: ==>> ");
+			switch (choice) {
 			case 1:
-				System.out.println("case 1");
+				System.out.println("\ncase 1\n");
 				break;
 			case 2:
-				System.out.println("case 2");
+				System.out.println("\ncase 2\n");
 				break;
 			case 3:
-				System.out.println("case 3");
+				System.out.println("\ncase 3\n");
 				break;
 			case 4:
-				System.out.println("case 4");
+				System.out.println("\ncase 4\n");
 				break;
 			case 0:
 				System.exit(0);
 			default:
-				System.out.println("Podaj prawid³owy numer");
+				System.out.println("Podaj prawid³owy numer!");
+				break;
 			}
 		}
 	}
