f85320578c944e478153d3ae7af69be5959ba842
Tomasz Bartnik <tomasz.bartnik93@gmail.com>
2016-06-05 20:17:42 +0200

    base of all menu options added

--- src/client/Client.java
+++ src/client/Client.java
@@ -10,8 +10,8 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
 import java.net.UnknownHostException;
-
 import client.EnterData;
+import Requests.*;
 
 public class Client {
 
@@ -19,9 +19,9 @@ public class Client {
 	private static EnterData enter;
 
 	static final String MENU = "M E N U\n"
-			+ "1 - wyswietl liste plikow i folderów\n" + "2 - dodaj plik\n"
-			+ "3 - usun plik\n" + "4 - pobierz plik\n"
-			+ "0 - zakoñcz program\n";
+			+ "1 - Wyswietl listê plików i folderów\n" + "2 - Dodaj plik\n"
+			+ "3 - Usuñ plik\n" + "4 - Pobierz plik\n"
+			+ "0 - Zakoñcz program\n";
 
 	public Client() {
 		clientId++;
@@ -58,35 +58,49 @@ public class Client {
 		enter = new EnterData("CON");
 		int choice;
 		Object obj = null;
-		String path;
+		String fileName;
 
 		while (true) {
 			System.out.println(MENU);
 			choice = enter.enterInt("Podaj swój wybór: ");
 			switch (choice) {
 			case 1:
+				/* Wyswietlanie listy plikow i katalogow */
 				obj = null;
 				try {
-					// nawiazywanie polaczenia z serwerem katalogowym
 					socketForCatalogServer = new Socket(HOST, KATALOG_SERVER_PORT);
 
 					oos = new ObjectOutputStream(socketForCatalogServer.getOutputStream());
 					ois = new ObjectInputStream(socketForCatalogServer.getInputStream());
 
-					// komunikacja
 					ConnectRequest connectRequest = new ConnectRequest(client.getClientId());
 					oos.writeObject(connectRequest);
 
-					while (!((obj = ois.readObject()) instanceof AcceptRequest)) {
-						System.out.println("Client: Waiting for connect request acceptation (Catalog Server)...");
+					while (!((obj = ois.readObject()) instanceof ConnectAccept)) {
+						System.out.println("Client: Waiting for the connect-request acceptation by Catalog Server...");
 					}
 					System.out.println("Client: Connected to Catalog Server.");
 					
+					// wysy³anie ¿¹dania o wyœwietlenie listy plików
 					FileListRequest fileListRequest = new FileListRequest();
 					oos.writeObject(fileListRequest);
-					// tutaj dodac odbieranie Listy plików, i wyswietlanie jej
+					System.out.println("Client: FileListRequest sent to Catalog Server");
 					
-						
+					// odbieranie i wyswietlanie listy plikow
+					while ((obj = ois.readObject()) != null) {
+						if (obj instanceof FileListRequest) 
+						{
+							FileListRequest fileListRequest1 = (FileListRequest) obj;
+							System.out.println("Client debug: " + fileListRequest1.getMessage());
+							
+							System.out.println("\nLista plików :\n"+ fileListRequest1.getPathList());
+						}
+						else
+						{
+							System.out.println("Client debug: Catalog Server sent wrong object");
+						}
+					}
+					
 					oos.close();
 					ois.close();
 					socketForCatalogServer.close();
@@ -102,17 +116,126 @@ public class Client {
 
 				break;
 			case 2:
-				System.out
-						.println("\nPodaj nazwê pliku, który chcesz wgraæ(pamiêtaj o padaniu ca³ej œcie¿ki np. root/folder1/folder2/plik.txt):\n");
+				/* Dodawanie pliku */
+				fileName = enter.enterString("\nPodaj nazwê pliku, który chcesz wgraæ(pamiêtaj o padaniu ca³ej œcie¿ki wraz z nazw¹ i rozszerzeniem pliku np. /root/folder1/folder2/plik.txt ):\n");
+				obj = null;
+				try {
+					// nawiazywanie polaczenia z serwerem katalogowym
+					socketForCatalogServer = new Socket(HOST, KATALOG_SERVER_PORT);
+
+					oos = new ObjectOutputStream(socketForCatalogServer.getOutputStream());
+					ois = new ObjectInputStream(socketForCatalogServer.getInputStream());
+
+					ConnectRequest connectRequest = new ConnectRequest(client.getClientId());
+					oos.writeObject(connectRequest);
+
+					while (!((obj = ois.readObject()) instanceof ConnectAccept)) {
+						System.out.println("Client: Waiting for the connect-request acceptation by Catalog Server...");
+					}
+					System.out.println("Client: Connected to Catalog Server.");
+					
+					// wys³anie ¿¹dania o dodanie pliku
+					AddFileRequest addFileRequest = new AddFileRequest();
+					oos.writeObject(addFileRequest);
+					System.out.println("Client: AddFileRequest sent to Catalog Server");
+					
+					/* TODO: 
+					 * otrzymywanie danych od SK o SP do polaczenia siê z nim
+					 * laczenie sie
+					 * wgrywanie pliku
+					 */
+					oos.close();
+					ois.close();
+					socketForCatalogServer.close();
+
+				} catch (UnknownHostException exc) {
+					
+				} catch (SocketException exc) {
+					
+				} catch (IOException exc) {
+					
+				}
+				
 				break;
 			case 3:
-				System.out
-						.println("\nPodaj nazwê pliku, który chcesz usun¹æ(pamiêtaj o padaniu ca³ej œcie¿ki np. root/folder1/folder2/plik.txt):\n");
+				/* Usuwanie pliku */
+				fileName = enter.enterString("\nPodaj nazwê pliku, który chcesz usun¹æ(pamiêtaj o padaniu ca³ej œcie¿ki wraz z nazw¹ i rozszerzeniem pliku np. /root/folder1/folder2/plik.txt ):\n");
+				obj = null;
+				try {
+					// nawiazywanie polaczenia z serwerem katalogowym
+					socketForCatalogServer = new Socket(HOST, KATALOG_SERVER_PORT);
+
+					oos = new ObjectOutputStream(socketForCatalogServer.getOutputStream());
+					ois = new ObjectInputStream(socketForCatalogServer.getInputStream());
+
+					ConnectRequest connectRequest = new ConnectRequest(client.getClientId());
+					oos.writeObject(connectRequest);
+
+					while (!((obj = ois.readObject()) instanceof ConnectAccept)) {
+						System.out.println("Client: Waiting for the connect-request acceptation by Catalog Server...");
+					}
+					System.out.println("Client: Connected to Catalog Server.");
+					
+					// wys³anie ¿¹dania o usuniecie pliku
+					DeleteFileRequest deleteFileRequest = new DeleteFileRequest();
+					oos.writeObject(deleteFileRequest);
+					System.out.println("Client: DeleteFileRequest sent to Catalog Server");
+					
+					oos.close();
+					ois.close();
+					socketForCatalogServer.close();
+
+				} catch (UnknownHostException exc) {
+					
+				} catch (SocketException exc) {
+					
+				} catch (IOException exc) {
+					
+				}
+				
+
 				break;
 			case 4:
-				System.out
-						.println("\nPodaj nazwê pliku, który chcesz pobraæ(pamiêtaj o padaniu ca³ej œcie¿ki np. root/folder1/folder2/plik.txt):\n");
-				/* DODAÆ WSTÊPN¥ KOMUNIKACJÊ Z SERWEREM KATALOGOWYM */
+				/* Pobieranie pliku */
+				fileName = enter.enterString("\nPodaj nazwê pliku, który chcesz pobraæ(pamiêtaj o padaniu ca³ej œcie¿ki wraz z nazw¹ i rozszerzeniem pliku np. /root/folder1/folder2/plik.txt ):\n");
+				obj = null;
+				try {
+					// nawiazywanie polaczenia z serwerem katalogowym
+					socketForCatalogServer = new Socket(HOST, KATALOG_SERVER_PORT);
+
+					oos = new ObjectOutputStream(socketForCatalogServer.getOutputStream());
+					ois = new ObjectInputStream(socketForCatalogServer.getInputStream());
+
+					ConnectRequest connectRequest = new ConnectRequest(client.getClientId());
+					oos.writeObject(connectRequest);
+
+					while (!((obj = ois.readObject()) instanceof ConnectAccept)) {
+						System.out.println("Client: Waiting for the connect-request acceptation by Catalog Server...");
+					}
+					System.out.println("Client: Connected to Catalog Server.");
+					
+					// wys³anie ¿¹dania o pobranie pliku
+					DownloadFileRequest downloadFileRequest = new DownloadFileRequest();
+					oos.writeObject(downloadFileRequest);
+					System.out.println("Client: DownloadFileRequest sent to Catalog Server");
+					
+					/* TODO:
+					 * otrzymywanie danych od SK o SP do polaczenia siê z nim
+					 * laczenie sie
+					 * pobieranie pliku 
+					 */
+					
+					oos.close();
+					ois.close();
+					socketForCatalogServer.close();
+
+				} catch (UnknownHostException exc) {
+					
+				} catch (SocketException exc) {
+					
+				} catch (IOException exc) {
+					
+				}
 
 				/* pocz¹tek komunikacji z Serwerem Plików */
 
